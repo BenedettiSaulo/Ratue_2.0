2b6830accfdd087908e00ed9486d74921eec862c
Saulo <saulo.benedetti@hotmail.com>
2023-11-05 18:10:39 -0300

    Criado projeto em Java e exportado para o GIT

---
+++ programa/ratue/ratue/src/test/java/br/edu/unoesc/ratue/ratue/RatueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.ratue.ratue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
